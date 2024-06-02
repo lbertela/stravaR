8a80816b4dd7c5f11b882199db512e8e7f1d5b2f
lbertela <ludovic.bertela@hotmail.com>
2024-06-02 11:24:42 +0200

    Modify main dataset

--- data_raw/get_strava_data.R
+++ data_raw/get_strava_data.R
@@ -1,10 +1,4 @@
-library(rStrava)
 library(dplyr)
-library(lubridate)
-# library(leaflet)
-# library(googleway)
-# library(htmlwidgets)
-# library(htmltools)
 
 ## Setup
 
@@ -14,16 +8,17 @@ app_client_id <- Sys.getenv("APP_CLIENT_ID")
 app_secret <- Sys.getenv("APP_SECRET")
 
 # Create strava token
-my_token <- httr::config(token = strava_oauth(app_name, app_client_id, app_secret,
-                                              app_scope = "activity:read_all"))
+my_token <- httr::config(token = rStrava::strava_oauth(
+     app_name, app_client_id, app_secret,
+     app_scope = "activity:read_all"))
 
 # Compile and clean up activity data
 activities <- rStrava::get_activity_list(stoken = my_token)
 my_acts <- rStrava::compile_activities(activities) %>% 
      
      # convert time from sec to hr/min/sec
-     mutate(moving_time = seconds_to_period(moving_time),
-            elapsed_time = seconds_to_period(elapsed_time)) %>% 
+     mutate(moving_time = lubridate::seconds_to_period(moving_time),
+            elapsed_time = lubridate::seconds_to_period(elapsed_time)) %>% 
      
      # convert start_date_local to Date object
      mutate(start_date_time_local = as_datetime(x = start_date_local),
@@ -39,10 +34,10 @@ my_acts <- rStrava::compile_activities(activities) %>%
      
      # rename sports
      mutate(sport_type = case_when(
-          sport_type == "MountainBikeRide" ~ "VTT",
-          sport_type == "Ride" ~ "Road Bike",
-          sport_type == "GravelRide" ~ "Gravel Bike",
-          sport_type == "Run" ~ "Run")) %>% 
+          sport_type == "MountainBikeRide" ~ "vtt",
+          sport_type == "Ride" ~ "road",
+          sport_type == "GravelRide" ~ "gravel",
+          sport_type == "Run" ~ "run")) %>% 
      
      # select, rename & reorder columns
      select(
@@ -53,6 +48,7 @@ my_acts <- rStrava::compile_activities(activities) %>%
           distance, total_elevation_gain, elev_high, 
           average_speed, max_speed, 
           moving_time, elapsed_time,
+          manual,
           # map info
           map.id, map.resource_state, map.summary_polyline, 
           start_latlng1, start_latlng2, end_latlng1, end_latlng2)
