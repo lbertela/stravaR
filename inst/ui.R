d63ac1ff2f9124c8fe9299778bce231f104c199e
lbertela <ludovic.bertela@hotmail.com>
2024-06-03 23:04:26 +0200

    Start creating app

---
+++ inst/ui.R
@@ -0,0 +1,26 @@
+library(shiny)
+library(shinydashboard)
+library(dplyr)
+devtools::load_all()
+
+ui <- dashboardPage(
+     skin = "blue",
+     dashboardHeader(title = "My Strava Dashboard"),
+     dashboardSidebar(
+          sidebarMenu(
+               menuItem("Heat Map", tabName = "heatmap",  icon = icon("location-dot")),
+               menuItem("Stats Overview", tabName = "stats", icon = icon("chart-line")),
+               menuItem("Data", tabName = "data", icon = icon("database"))
+          )
+     ),
+     dashboardBody(
+          fluidRow(
+               tags$head(
+                    tags$link(rel = "stylesheet", type = "text/css", href = "custom_valueBox.css")
+               ),
+               valueBox(nrow(my_acts), "Activities", icon = icon("strava"), color = "blue"),
+               valueBox(format(round(sum(my_acts$distance), digits = 0), big.mark = "'"), "Kms", icon = icon("globe"), color = "green"),
+               valueBox(format(round(sum(my_acts$total_elevation_gain), digits = 0), big.mark = "'"), "Meters of elevation", icon = icon("arrow-trend-up"), color = "purple")
+          )
+     )
+)
