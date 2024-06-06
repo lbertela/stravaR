1830fb9cce70e1bb4ab6068216b71ef7658fb60b
lbertela <ludovic.bertela@hotmail.com>
2024-06-06 21:18:55 +0200

    Better rendering of UI

--- inst/ui.R
+++ inst/ui.R
@@ -19,18 +19,20 @@ ui <- dashboardPage(
           tags$head(
                tags$link(rel = "stylesheet", type = "text/css", href = "custom_valueBox.css")
           ),
+          valueBox(nrow(my_acts), "Activities", icon = icon("strava"), color = "blue"),
+          valueBox(format(round(sum(my_acts$distance), digits = 0), big.mark = "'"), "Kms", icon = icon("globe"), color = "green"),
+          valueBox(format(round(sum(my_acts$total_elevation_gain), digits = 0), big.mark = "'"), "Meters of elevation", icon = icon("arrow-trend-up"), color = "purple"),
           tabItems(
                tabItem(
+                    align = "center",
                     tabName = "heatmap",
-                    leafletOutput("mymap")
+                    leafletOutput("mymap", width="60%", height="600px")
                ),
                tabItem(
-                    tabName = 'stats',
-                    fluidRow(
-                         valueBox(nrow(my_acts), "Activities", icon = icon("strava"), color = "blue"),
-                         valueBox(format(round(sum(my_acts$distance), digits = 0), big.mark = "'"), "Kms", icon = icon("globe"), color = "green"),
-                         valueBox(format(round(sum(my_acts$total_elevation_gain), digits = 0), big.mark = "'"), "Meters of elevation", icon = icon("arrow-trend-up"), color = "purple")
-                    )
+                    tabName = 'stats'
+               ),
+               tabItem(
+                    tabName = 'data'
                )
           )
      )
